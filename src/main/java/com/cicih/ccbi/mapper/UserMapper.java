1c91b1b8940d96160a703f131ecaf0f466908aa8
zoeci2523 <13192677392@163.com>
2024-05-04 12:07:13 +0800

    Initial commit

---
+++ src/main/java/com/cicih/ccbi/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cicih.ccbi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cicih.ccbi.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
